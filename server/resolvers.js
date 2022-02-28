d584c72025c2fcef7e489ca04c5fbe609bbe84bb
Jacques Laforest <adxmgr@gmail.com>
2022-02-27 21:13:34 -0500

    lecture 21 - completed

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,13 +1,18 @@
 const db = require("./db");
 
 const Query = {
-  company:(root, {id}) => db.companies.get(id),
-  job: (root, {id}) => db.jobs.get(id),
+  company: (root, { id }) => db.companies.get(id),
+  job: (root, { id }) => db.jobs.get(id),
   jobs: () => db.jobs.list(),
 };
 
-const Job = {
-  company: (job) => db.companies.get(job.companyId)
+const Company = {
+  jobs: (company) =>
+    db.jobs.list().filter((job) => job.companyId === company.id),
 };
 
-module.exports = { Query, Job };
+const Job = {
+  company: (job) => db.companies.get(job.companyId),
+};
+
+module.exports = { Query, Company, Job };
